7687b6f63791242b01a9033a4d1c28b0d6bc18ae
Bl4omArchie <grlch.alexandre@proton.me>
2024-02-19 17:27:57 +0100

    remove function from tricks to util

--- scripts/util.py
+++ scripts/util.py
@@ -1,6 +1,16 @@
-def get_length(num):
+#Count the number of bits
+def get_bit_length(num):
     length = 0
     while num:
         length += 1
         num >>= 1
     return length
+
+
+#Count the number of bits equal to 1
+def bit_count(a):
+    compt=0
+    while (a):
+        compt += a&1
+        a>>=1
+    return compt
